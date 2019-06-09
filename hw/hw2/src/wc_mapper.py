7b2a3dbd2971c0cbe324c4c124f8c007c4775ba4
Rong Feng <rf1316@login-1-1.local>
2019-06-09 12:07:16 -0400

    hw2

---
+++ hw/hw2/src/wc_mapper.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import sys
+
+match_list = ['hackathon', 'Dec', 'Chicago', 'Java']
+
+for l in sys.stdin:
+    for word in match_list:
+        if word.lower() in l.lower():
+            print "%s\t%s" % (word, 1)
